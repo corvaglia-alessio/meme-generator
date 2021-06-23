13c1f5208cd6459f1c9125b0dadbe77671bc57c0
Alessio Corvaglia <s281557@studenti.polito.it>
2021-06-23 22:16:12 +0200

    minor fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import {Navigation} from './components/Navigation.js'
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
