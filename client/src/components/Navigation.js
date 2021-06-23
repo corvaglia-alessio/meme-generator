c47c92cf9ae0785c96e63d9ef101250cfbb98ed7
Alessio Corvaglia <s281557@studenti.polito.it>
2021-06-23 23:22:27 +0200

    navbar

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,12 +1,17 @@
-import {Navbar, Nav} from 'react-bootstrap/';
+import {Navbar, Nav, Button} from 'react-bootstrap/';
+import {DoorOpen, AspectRatio} from 'react-bootstrap-icons';
 
 function Navigation(props){
     return(
         <>
-            <Navbar bg="warning" variant="dark" fixed="top">
+            <Navbar bg="warning" fixed="top">
+                <AspectRatio className="mr-2" size= "30"/>
                 <Navbar.Brand href="">MemeGenerator</Navbar.Brand>
                 <Nav className="mr-auto">
-                    <Nav.Link href="">Login</Nav.Link>
+                    <Button variant="success">
+                        <DoorOpen className="mr-2" size="30"/>
+                         Login
+                    </Button>
                 </Nav>
             </Navbar>
         </>
