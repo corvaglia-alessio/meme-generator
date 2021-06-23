3153a06cfe668ca3f67a0898806727262e403499
Alessio Corvaglia <s281557@studenti.polito.it>
2021-06-23 23:44:35 +0200

    completed navbar items and inserted modal

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,20 +1,29 @@
-import {Navbar, Nav, Button} from 'react-bootstrap/';
-import {DoorOpen, AspectRatio} from 'react-bootstrap-icons';
+import {Navbar, Nav, Button, Container} from 'react-bootstrap/';
+import {DoorOpen, DoorClosed, AspectRatio, PlusCircle} from 'react-bootstrap-icons';
 
 function Navigation(props){
     return(
-        <>
+        <Container fluid>
             <Navbar bg="warning" fixed="top">
                 <AspectRatio className="mr-2" size= "30"/>
                 <Navbar.Brand href="">MemeGenerator</Navbar.Brand>
-                <Nav className="mr-auto">
+                <Nav className="ml-auto">
+                    <Button variant="success">
+                        <PlusCircle className="mr-2" size="30"/>
+                         Create new meme
+                    </Button>
                     <Button variant="success">
                         <DoorOpen className="mr-2" size="30"/>
                          Login
                     </Button>
+                    <Button variant="danger">
+                        <DoorClosed className="mr-2" size="30"/>
+                         Logout
+                    </Button>
+                    <Navbar.Brand href="">Welcome Tizio!</Navbar.Brand>
                 </Nav>
             </Navbar>
-        </>
+        </Container>
     )
 }
 
